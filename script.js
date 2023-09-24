ba9fa833f132c70584260398f1b1f74f151b07bb
postive-vibes-2023 <gaurav0133@gmail.com>
2023-09-24 21:45:51 +0100

    First fully functional version. First fully functional version is complete. We will merge it to main branch and then delete this branch (may be). 24/09/2023. The code is working fine. Next is improving the styling and thinking of adding more functions.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-//** define the constants. **
+//Declare constants
+// -------------------------------------
+
 const body = document.querySelector('body');
 const gridContainer = document.querySelector('.grid-container');
 const input = document.getElementById('number-input');
@@ -8,12 +10,13 @@ const eraser = document.getElementById('erase-button');
 const gridWidth = gridContainer.clientWidth;
 const gridHeight = gridContainer.clientHeight;
 
-let boxes = null; //declare boxes a node list in global scope so that it can be accessed by all function in the script. its initial value is null (as no boxes to start with). Once fillGrid functions runs (Initially on page load and later on pressing submit button), this function not only creates boxes but also updates the value of boxes in global scope so that it can be access by other functions.
+let boxes = null; //declare boxes - a node list in global scope so that it can be accessed by all functions in the script. its initial value is null (as no boxes to start with). Once fillGrid functions runs (Initially on page load and later on pressing submit button), this function not only creates boxes but also updates the value of boxes in global scope so that it can be access by other functions.
 //constants used for toggle functionality.
 let isPainting = false;
 let isErasing = false;
 
-//** FUNCTIONS **
+//** Define functions.
+// -------------------------------------
 
 // define function to fill the girdContainer with desire number of boxes.
 // Default is 16X16=256 boxes.
@@ -24,7 +27,6 @@ function fillGrid() {
 	} else {
 		n = 16;
 	}
-	console.log(n);
 	gridSize = n * n;
 	var i = 1; // setting the counter to 1; it will increase till i= gridSize. Use while loop.
 
@@ -35,56 +37,116 @@ function fillGrid() {
 		var gridArea = gridHeight * gridWidth; //calculate the available area / space in gridContainer.
 		var boxElementArea = gridArea / gridSize; //the area of each box = area of girdContainer divided by the number of boxes requested.
 		var boxSize = Math.sqrt(boxElementArea); // each dimension of the square box = sqrt of the box area.
-
 		boxElement.style.flex = '1 1 ' + boxSize + 'px'; // set the inline style for flex property setting flex.grow to 1 , flex-shrink to and flex basis = boxSize in px.
 		boxElement.style.height = boxSize + 'px'; // set box height = boxSize (ensuring each box is a square)
 		gridContainer.appendChild(boxElement); // append the boxElement created to the gird container.
 		i++;
 	}
 	const newBoxes = document.querySelectorAll('.box');
-	boxes = newBoxes;
-	// sketch(); // function to allow sketching / painting using the cursor
-	console.log(boxes);
+	boxes = newBoxes; // pass the value of newBoxes outside the function.
 }
 
-//define function to clear old grid each time the user request new grid.
+//Define function to clear old grid each time the user request new grid.
 function clearGrid() {
 	if (boxes) {
 		boxes.forEach((box) => {
 			box.remove();
 		});
 	} else {
-		console.log('Nothing to clear!');
 	}
 }
+
+// Define function to handle pen. (pemDown= mousedown; penUp=mouseup ; penMove=m
+// mousemove events respectively)
 function penDownHandler(e) {
-	console.log('pen down');
 	if (e.button === 0) {
-		console.log('left click');
 		// Left mouse button is clicked (button === 0)
 		isPainting = true;
 	}
 }
 function penUpHandler() {
-	console.log('pen up');
 	pen.textContent = 'Click and drag to draw';
 	isPainting = false;
 }
 function penMoveHandler(e) {
-	console.log(`logig the status of pen inside movehandler ${isPainting}`);
 	if (isPainting) {
 		pen.textContent = 'Drawing!';
 		e.target.style.cursor = 'pointer';
 		e.target.style.backgroundColor = 'black';
-		console.log('pen moving');
+	}
+}
+//Define function to remove pen related event handlers (drawing tools)
+function removePenEventListeners() {
+	if (pen.textContent === 'Click and drag to draw') {
+		gridContainer.removeEventListener('mousedown', penDownHandler);
+		gridContainer.removeEventListener('mouseup', penUpHandler);
+		boxes.forEach((box) => {
+			box.removeEventListener('mousemove', penMoveHandler);
+		});
 	}
 }
 
+// Define functions to handle eraser related tools.
+function eraserDownHandler(e) {
+	if (e.button === 0) {
+		// Left mouse button is clicked (button === 0)
+		isErasing = true;
+	}
+}
+function eraserUpHandler() {
+	eraser.textContent = 'Click and drag to erase';
+	isErasing = false;
+}
+function eraserMoveHandler(e) {
+	if (isErasing) {
+		eraser.textContent = 'Erasing!';
+		e.target.style.cursor = 'pointer';
+		e.target.style.backgroundColor = '';
+	}
+}
+
+//Define function to remove eraser related event handlers (Eraser tools)
+function removeEraserEventListeners() {
+	if (eraser.textContent === 'Click and drag to erase') {
+		gridContainer.removeEventListener('mousedown', eraserDownHandler);
+		gridContainer.removeEventListener('mouseup', eraserUpHandler);
+		boxes.forEach((box) => {
+			box.removeEventListener('mousemove', eraserMoveHandler);
+		});
+	}
+}
+
+// ** Call relevant functions on page load. **
+//---------------------------------------------------------
+
+fillGrid(); //  // call function to fill the  grid outline as soon as page loads. It fill will default 256 boxes.
+
+// Finally add event listeners to different buttons to add  ability for user
+// interaction.
+//---------------------------------------------------------
+
+// SUbmit button.
+submit.addEventListener('click', () => {
+	clearGrid(); // clearGrid function is called upon as soon as the user submit new input.
+	if (eraser.textContent === 'Click and drag to erase') {
+		removeEraserEventListeners();
+		eraser.textContent = 'Eraser';
+	}
+	if (pen.textContent === 'Click and drag to draw') {
+		removePenEventListeners();
+		pen.textContent = 'Pen';
+	}
+	setTimeout(() => {
+		fillGrid(); // execute the fillGrid function after some delay for aesthetic purposes.
+	}, 100);
+});
+
+//Pen button (Drawing tools)
 pen.addEventListener('click', (e) => {
 	if (pen.textContent === 'Pen') {
 		removeEraserEventListeners();
 		pen.textContent = 'Click and drag to draw';
-		eraser.textContent = 'Pen';
+		eraser.textContent = 'Eraser';
 		gridContainer.addEventListener('mousedown', penDownHandler);
 		gridContainer.addEventListener('mouseup', penUpHandler);
 		boxes.forEach((box) => {
@@ -93,43 +155,7 @@ pen.addEventListener('click', (e) => {
 	}
 });
 
-function removePenEventListeners() {
-	if (pen.textContent === 'Click and drag to draw') {
-		gridContainer.removeEventListener('mousedown', penDownHandler);
-		gridContainer.removeEventListener('mouseup', penUpHandler);
-		console.log(
-			`logging boxes from inside the remove event listner function${boxes}`
-		);
-		boxes.forEach((box) => {
-			console.log(box);
-			box.removeEventListener('mousemove', penMoveHandler);
-		});
-	}
-}
-
-function eraserDownHandler(e) {
-	console.log('eraser down');
-	if (e.button === 0) {
-		console.log('left click');
-		// Left mouse button is clicked (button === 0)
-		isErasing = true;
-	}
-}
-function eraserUpHandler() {
-	console.log('Eraser up');
-	eraser.textContent = 'Click and drag to erase';
-	isErasing = false;
-}
-function eraserMoveHandler(e) {
-	console.log(`logging the status of eraser inside move handler ${isPainting}`);
-
-	if (isErasing) {
-		eraser.textContent = 'Erasing!';
-		e.target.style.cursor = 'pointer';
-		e.target.style.backgroundColor = '';
-		console.log('Eraser is moving');
-	}
-}
+//Eraser button (Erasing tools)
 eraser.addEventListener('click', (e) => {
 	if (eraser.textContent === 'Eraser') {
 		removePenEventListeners();
@@ -142,40 +168,7 @@ eraser.addEventListener('click', (e) => {
 			box.addEventListener('mousemove', eraserMoveHandler);
 		});
 	}
-
-	function removeEraserEventListeners() {
-		if (eraser.textContent === 'Click and drag to erase') {
-			gridContainer.removeEventListener('mousedown', eraserDownHandler);
-			gridContainer.removeEventListener('mouseup', eraserUpHandler);
-			console.log(
-				`logging boxes from inside the remove event listner function${boxes}`
-			);
-			boxes.forEach((box) => {
-				box.removeEventListener('mousemove', eraserMoveHandler);
-			});
-		}
-	}
 });
-//define function to enable painting of the boxes.
-function sketch() {
-	// add mousedown event listener to the **document** itself.
-	gridContainer.addEventListener('mousedown', penDownHandler);
-	// toggle of isPainting when mouse is up.
-	gridContainer.addEventListener('mouseup', penDownHandler);
-	// now add mousemove event listener to each box.
-	boxes.forEach((box) => {
-		box.addEventListener('mousemove', penMoveHandler(box));
-	});
-}
-// ** Call functions. **
 
-fillGrid(); //  // call function to fill the  grid outline as soon as page loads. It fill will default 256 boxes.
-
-// add event listener submit button. This will first clear the grid and then
-// after 100ms gap will fill the grid with requested number of boxes.
-submit.addEventListener('click', () => {
-	clearGrid(); // clearGrid function is called upon as soon as the user submit new input.
-	setTimeout(() => {
-		fillGrid(); // execute the fillGrid function after some delay for aesthetic purposes.
-	}, 100);
-});
+// 24/09/2023. The code is working fine. Next is improving the styling and
+// thinking of adding more functions.
